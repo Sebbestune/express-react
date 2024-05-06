d7aba4fdf019088170279428234f6a5366082c21
Sebastian Lindgren <sebastian.tb.lindgren@gmail.com>
2024-05-06 11:47:05 +0300

    API config

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,6 +3,9 @@ import bookRouter from "./routes/books";
 
 const app: Express = express();
 
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
 app.use("/books", bookRouter);
 
 app.get("/", (req: Request, res: Response) => {
