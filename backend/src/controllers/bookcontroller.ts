60742e49e8913d72294fb5623a0718550d0fbb03
Sebastian Lindgren <sebastian.tb.lindgren@gmail.com>
2024-05-08 11:45:40 +0200

    Controller for Book

--- backend/src/controllers/bookcontroller.ts
+++ backend/src/controllers/bookcontroller.ts
@@ -1,28 +1,54 @@
+import { IBook } from "../interfaces/IBook";
+import Book from "../models/bookModel";
 
-
-const create = async () => {
-
-}
+const create = async (data: IBook) => {
+  return await Book.create(data);
+};
 const readAll = async () => {
+  const books = await Book.find({});
+  return books;
+};
+const read = async (id: any) => {
+  return await Book.findById(id);
+};
+const update = async (id: any, data: IBook) => {
+  return await Book.findByIdAndUpdate(id, data, { new: true });
+};
+const deleteOne = async (id: any) => {
+  return await Book.findByIdAndDelete(id);
+};
 
-}
-const read = async () => {
+export const getAllBooks = async (req: any, res: any) => {
+  const books = await readAll();
+  res.status(200).json(books);
+};
+export const addBook = async (req: any, res: any) => {
+  const { title, author, publisher, read } = req.body;
 
-}
-const update = async () => {
+  const result = await create({ title, author, publisher, read });
 
-}
-const deleteOne = async () => {
+  res.status(201).json({ message: "Book created successfully", book: result });
+};
+export const getBook = async (req: any, res: any) => {
+  const id = req.params.id;
+  const book = await read(id);
 
-}
+  res.status(200).json(book);
+};
+export const updateBook = async (req: any, res: any) => {
+  const { title, author, publisher, read } = req.body;
+  const id = req.params.id;
 
-export const getAllBooks = (req: any, res: any) => {
-}
-export const addBook = (req: any, res: any) => {
-}
-export const getBook = (req: any, res: any) => {
-}
-export const updateBook = (req: any, res: any) => {
-}
-export const deleteBook = (req: any, res: any) => {
-}
+  const updatedBook = await update(id, { title, author, publisher, read });
+
+  res
+    .status(200)
+    .json({ message: "Update succeeded", updatedBook: updatedBook });
+};
+export const deleteBook = async (req: any, res: any) => {
+  const id = req.params.id;
+
+  const deletedBook = await deleteOne(id);
+
+  res.status(200).json({ message: "Book deleted", deletedBook: deletedBook });
+};
