3bea4d2dd417f39317fa149b8034b2c93d97addd
Sebastian Lindgren <sebastian.tb.lindgren@gmail.com>
2024-05-08 10:37:20 +0200

    Start of Mongoose implementation

--- backend/src/controllers/bookcontroller.ts
+++ backend/src/controllers/bookcontroller.ts
@@ -1,16 +1,28 @@
 
+
+const create = async () => {
+
+}
+const readAll = async () => {
+
+}
+const read = async () => {
+
+}
+const update = async () => {
+
+}
+const deleteOne = async () => {
+
+}
+
 export const getAllBooks = (req: any, res: any) => {
-    res.json({totalBooks: 2, books: [
-        {
-            author: "Sebbe",
-            title: "The green book",
-            pages: 394
-        },
-        {
-            author: "Tomas",
-            title: "The red book",
-            pages: 584
-        },
-        
-    ]});
+}
+export const addBook = (req: any, res: any) => {
+}
+export const getBook = (req: any, res: any) => {
+}
+export const updateBook = (req: any, res: any) => {
+}
+export const deleteBook = (req: any, res: any) => {
 }
--- backend/src/routes/books.ts
+++ backend/src/routes/books.ts
@@ -3,6 +3,10 @@ import { getAllBooks } from "../controllers/bookcontroller";
 
 const bookRouter = Router();
 
-bookRouter.get("/", getAllBooks)
+bookRouter.get("/", getAllBooks);
+bookRouter.post("/", addBook);
+bookRouter.get("/:id", getBook);
+bookRouter.put("/:id", updateBook);
+bookRouter.delete("/:id", deleteBook);
 
 export default bookRouter;
